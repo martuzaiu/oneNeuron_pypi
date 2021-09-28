b0e8ee22d5026e9e258a6479a7dcd9da808d21b5
martuza <martuza.cse@gmail.com>
2021-09-28 20:54:52 +0600

    read me updated

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # oneNeuron_pypi
 oneNeuron_pypi
 
+# How to use this
+get X and y then follow the below command
+
+```python
+from oneNeuron.perceptron import Perceptron
+model = Perceptron(eta=eta, epochs=epochs)
+model.fit(X,y)
+```
+
 
 
 # COMMANDS
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "martuzaiu"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="martuza.cse@gmail.com",
     description="A simple perceptron program",

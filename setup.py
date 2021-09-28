d4c5aefcbd1fd1d6510f585661b13b4d20b4032a
martuza <martuza.cse@gmail.com>
2021-09-28 17:22:33 +0600

    another try

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "numpy",
         "tqdm",
